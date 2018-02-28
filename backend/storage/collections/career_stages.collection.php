b9bd004f42b4ed272262f86294b3de84fb3cfcc3
Sven Petersen <s.petersen@webever.de>
2018-02-28 19:45:54 +0100

    cockpit database WIP

---
+++ backend/storage/collections/career_stages.collection.php
@@ -0,0 +1,123 @@
+<?php
+ return array (
+  'name' => 'career_stages',
+  'label' => 'Career Stages',
+  '_id' => 'career_stages5a96f6b0a05cf',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'job_title',
+      'label' => 'Job Title',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+      'required' => true,
+    ),
+    1 => 
+    array (
+      'name' => 'company',
+      'label' => 'Company',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'description',
+      'label' => 'Description',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'from',
+      'label' => 'From',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    4 => 
+    array (
+      'name' => 'to',
+      'label' => 'To',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1519842992,
+  '_modified' => 1519843001,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
