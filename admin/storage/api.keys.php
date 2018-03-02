f8e007d44e902dff19a168c3280cc1efe4393a2f
Sven Petersen <s.petersen@webever.de>
2018-03-02 19:19:18 +0100

    renamed js file

--- admin/storage/api.keys.php
+++ admin/storage/api.keys.php
@@ -1,2 +1,2 @@
 <?php
- return '¢z>ºgVJÐ¢i3—Uü#–T`xWÊÞê£¦›	–Ø¹ðûfOÜÙT%´Õ]º3Óî$ÒU3%³"Ãm„g=ºu$Ð' . "\0" . '#1#wzËËû˜Ë˜}êUgæíÂæ"ŒLœ¦¥½%¬úJçPVCD¼à•³€_òØ»¦(úrß‹ì¸ºO$÷´<ˆ§<î®àäú=…ŽÁ: Ët=ƒ{5’ÈtÔ¯)*_ý¿È#ÂcTO9¬ÌXW)ÔÖ¨œwË¯qÆÉàè‚2û4Zž*Ø%ƒÆFâü\\\\•«OBò¬Ÿ7bmroã½”á®!`\'¢þÂ3\'§%¸×¬Ëâ"=ûöuuPîÃX¢UŸ7±ë';
+ return '¢z>ºgVJÐ¢i3—Uü#–T`xWÊÞê£¦›	–Ø¹ðûfOÜÙT%´Õ]º3Óî$ÒU3%³"Ãm„g=ºu$Ð' . "\0" . '#1#wzËËû˜Ë˜}êUgæíÂæ"ŒLœ¦¥½%¬úJçPVCD¼à•³€_òØ»¦(úrß‹ì¸ºO$÷´<„¡<î®àäú=…ŽÁ: Ët=ƒ{5’ÈtÔ¯)*_ý¿È#ÂcTO9¬ÌXW)ÔÖ¨œwË¯qÆÉàÀ– ±iOÎ&Ñ*ƒ_Ž´¡å¾¾ICèºû|Î4;?|móòÒàä\'asãªÝevþs¸Ö¤×ì9h¾¨&]TäÁ_¢Ù,%œß_)[âN';
