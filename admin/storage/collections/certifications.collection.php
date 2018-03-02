c2a30ece28ef44eca25e807de49a73ce65eee48d
Sven Petersen <s.petersen@webever.de>
2018-03-02 19:01:06 +0100

    cockpit data

---
+++ admin/storage/collections/certifications.collection.php
@@ -0,0 +1,50 @@
+<?php
+ return array (
+  'name' => 'certifications',
+  'label' => 'Certifications',
+  '_id' => 'certifications5a9988412d534',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'name',
+      'label' => 'Name',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1520011329,
+  '_modified' => 1520011329,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
