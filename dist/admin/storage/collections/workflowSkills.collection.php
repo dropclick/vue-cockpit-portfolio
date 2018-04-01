28869996bd8a9991857dd18c09925d6efc8eafbb
Sven Petersen <s.petersen@webever.de>
2018-04-01 22:24:24 +0200

    skills from cockpit

---
+++ dist/admin/storage/collections/workflowSkills.collection.php
@@ -0,0 +1,50 @@
+<?php
+ return array (
+  'name' => 'workflowSkills',
+  'label' => 'Workflow Skills',
+  '_id' => 'workflowSkills5ab288b8c5b7d',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'name',
+      'label' => 'Name',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1521649848,
+  '_modified' => 1521649867,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
